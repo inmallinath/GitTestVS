7d9943a7584b2716af6450155d32d0ccf3b5739d
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-14 16:09:50 -0700

    Define property and Methods

--- TestRepo.BL/Customer.cs
+++ TestRepo.BL/Customer.cs
@@ -8,5 +8,31 @@ namespace TestRepo.BL
 {
     public class Customer
     {
+        private string _lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string FirstName { get; set; }
+        public string emailAddress { get; set; }
+
+        /// <summary>
+        /// Concatenates Firstname and Lastname together 
+        /// separating them with a ","
+        /// </summary>
+        /// <returns></returns>
+        public string FullName()
+        {
+            return LastName + ", " + FirstName;
+        }
     }
 }
